37d3e1700dd9038b700b87680c7b6c1fcd0923d3
Arif Khan <arifkhan@nimapinfotech.com>
2023-06-07 10:04:47 +0530

    -> Implement Stored Procedure file in Data Model

---
+++ SurveyTask/DataModel/sp_GetReport.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROC sp_GetReport    
+ @DateRange VARCHAR(100),    
+ @State VARCHAR(50),    
+ @District VARCHAR(50),    
+ @Taluka VARCHAR(50),    
+ @Season VARCHAR(50)    
+AS    
+BEGIN    
+ SELECT * FROM SurveyData as sd    
+ WHERE    
+  (@DateRange IS NULL OR sd.uploaded_date LIKE '%' + @DateRange + '%')    
+  AND (@State IS NULL OR sd.[state] LIKE '%' + @State + '%')    
+  AND (@District IS NULL OR sd.dist LIKE '%' + @District + '%')    
+  AND (@Taluka IS NULL OR sd.taluka LIKE '%' + @Taluka + '%')    
+  AND (@Season IS NULL OR sd.season LIKE '%' + @Season + '%')    
+END
